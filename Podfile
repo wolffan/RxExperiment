861ca0d8544a62bc8316a601a52dafb7cc57e70c
Raimon Lapuente <r.lapuente@gmail.com>
2016-02-28 00:35:41 +0100

    Install RxSwift Setup Podfile

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'RxExperiment' do
+  pod 'RxSwift', '~> 2.2'
+end
---
+++ Pods/Target Support Files/Pods-RxExperiment/Pods-RxExperiment-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RxExperimentVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RxExperimentVersionString[];
+
